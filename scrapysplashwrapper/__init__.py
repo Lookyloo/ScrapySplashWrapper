d0e129f9962098603f6686c8152030f7d31a604a
Raphaël Vinot <raphael@vinot.info>
2019-01-29 10:29:52 +0100

    chg: add fallback for UA

--- scrapysplashwrapper/__init__.py
+++ scrapysplashwrapper/__init__.py
@@ -14,6 +14,8 @@ def crawl(splash_url, url, depth=1,
         queue.put(res)
 
     q = multiprocessing.Queue()
+    if not user_agent:
+        user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36'
     p = multiprocessing.Process(target=_crawl, args=(q, splash_url, user_agent, url,
                                                      depth, log_enabled, log_level))
     p.start()
