11d143cd760083b62675d2c94d46b78c572c4bcb
Raphaël Vinot <raphael@vinot.info>
2019-12-26 18:21:02 +0100

    chg: Add comments to the crawl method.

--- scrapysplashwrapper/__init__.py
+++ scrapysplashwrapper/__init__.py
@@ -3,11 +3,13 @@ __version__ = '0.1'
 from .middleware import ScrapySplashWrapperDepthMiddleware  # noqa
 from .crawler import ScrapySplashWrapperCrawler
 import multiprocessing
+from typing import List
 
 
-def crawl(splash_url, url, depth=1,
-          user_agent='Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36',
-          log_enabled=False, log_level='WARNING'):
+def crawl(splash_url: str, url: str, depth: int=1,
+          user_agent: str='Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36',
+          log_enabled: bool=False, log_level: str='WARNING') -> List[dict]:
+    '''Send the URL to crawl to splash, returns a list of responses from splash. Each entry from the list corresponds to a single URL loaded by Splash.'''
     def _crawl(queue, splash_url, ua, url, depth, log_enabled, log_level):
         crawler = ScrapySplashWrapperCrawler(splash_url, ua, depth, log_enabled, log_level)
         res = crawler.crawl(url)
