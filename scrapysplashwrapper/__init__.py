4ff722ca3ebb072fcdee424c25de678332774149
Raphaël Vinot <raphael@vinot.info>
2020-01-23 10:29:50 +0100

    chg: Improve typing, mark library as supporting it.

--- scrapysplashwrapper/__init__.py
+++ scrapysplashwrapper/__init__.py
@@ -3,11 +3,11 @@ __version__ = '0.1'
 from .middleware import ScrapySplashWrapperDepthMiddleware  # noqa
 from .crawler import ScrapySplashWrapperCrawler
 import multiprocessing
-from typing import List
+from typing import List, Optional
 
 
 def crawl(splash_url: str, url: str, cookies: List[dict]=[], depth: int=1,
-          user_agent: str='Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36',
+          user_agent: Optional[str]='Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36',
           log_enabled: bool=False, log_level: str='WARNING') -> List[dict]:
     '''Send the URL to crawl to splash, returns a list of responses from splash. Each entry from the list corresponds to a single URL loaded by Splash.'''
     def _crawl(queue, splash_url, ua, url, cookies, depth, log_enabled, log_level):
