383fafad20a111e02fa53dc639b4cc53c7b8456c
Raphaël Vinot <raphael@vinot.info>
2020-03-20 16:37:09 +0100

    chg: Bump version in setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup  # type: ignore
 
 setup(
     name='ScrapySplashWrapper',
-    version='0.1',
+    version='1.0',
     author='Raphaël Vinot',
     author_email='raphael.vinot@circl.lu',
     maintainer='Raphaël Vinot',
