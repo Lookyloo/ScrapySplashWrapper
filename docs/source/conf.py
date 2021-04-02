a3c7079cbaa637f6d54aa9e1bf676608fe56bcef
Raphaël Vinot <raphael@vinot.info>
2021-04-02 14:58:46 +0200

    chg: Bump version

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,7 @@ copyright = '2021, Lookyloo team'
 author = 'Lookyloo team'
 
 # The full version, including alpha/beta/rc tags
-release = '1.4.0'
+release = '1.5.0'
 
 
 # -- General configuration ---------------------------------------------------
